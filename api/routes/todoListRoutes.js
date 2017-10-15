c306d57dc176169daebaeb2bc24372ac740588aa
Albert Yanit <ayanit1@googlemail.com>
2017-10-15 17:43:27 +0100

    Set up routes

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,14 @@
+'use strict';
+module exports = (app) => {
+  const todoList = require('../controllers/todoListController')
+
+  // todoList Routes
+  app.route('/tasks')
+     .get(todoList.list_all_tasks)
+     .post(todoList.create_a_task);
+
+  app.route('/task/:taskId')
+     .get(todoList.read_a_task)
+     .put(todoList.update_a_task)
+     .delete(todoList.delte_a_task);
+};
